1000be39624c25108b6ab707e8076f1170f5b9ee
Andreas Nilsson <andreas@webzone.se>
2015-02-22 02:19:19 +0100

    Added construct to CTable (test)

--- src/HTMLTable/CTable.php
+++ src/HTMLTable/CTable.php
@@ -1,1 +1,20 @@
 <?php
+
+namespace Ann\HTMLTable;
+
+/**
+ * A utility class to easy creating and handling of tables.
+ * 
+ * @package CTable
+ */
+ 
+class CTable
+{
+
+		public function __construct()
+		{
+				echo 'Class CTable loaded.';
+		
+		}
+
+}
