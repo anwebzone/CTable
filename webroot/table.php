ed4c586d0ccc1681118c020e5f8a026e703acc25
Andreas Nilsson <andreas@webzone.se>
2015-02-25 00:45:30 +0100

    updated

--- webroot/table.php
+++ webroot/table.php
@@ -44,22 +44,22 @@ $table = new \Ann\HTMLTable\CTable($data, $remove, 'table-css-class');
 	</style>
 </head>
 <body>
-	<h1>CTable Example: How to create HTML table with CTable.</h1>
-	<p>First of all CTable needs some data to display. This below is how my array $data looks like. This is the content I want to show in a html table.</p>
-	<pre>
-	<?=var_dump($data)?>
-	</pre>
-	<h2>I don't want to show the array key "password" to everyone.</h2>
-	<p>To remove the "password" key I add the key name in another array $remove = ['password'];</p>
-	<pre>
-	<?=var_dump($remove)?>
-	</pre>
-	<h2>Create and display table</h2>
-	<p><pre><code>
+<h1>CTable Example: How to create HTML table with CTable.</h1>
+<p>First of all CTable needs some data to display. This below is how my array $data looks like. This is the content I want to show in a html table.</p>
+<pre>
+<?=var_dump($data)?>
+</pre>
+<h2>I don't want to show the array key "password" to everyone.</h2>
+<p>To remove the "password" key I add the key name in another array $remove = ['password'];</p>
+<pre>
+<?=var_dump($remove)?>
+</pre>
+<h2>Create and display table</h2>
+<p><pre><code>
 $table = new \Ann\HTMLTable\CTable($data, $remove, 'table-css-class');
 echo $table->table;
-	</code></pre></p>
-	<?=$table->table?>
-	<p>As you see above "password" never showed up cause CTable removed it from $data. </p>
+</code></pre></p>
+<?=$table->table?>
+<p>As you see above "password" never showed up cause CTable removed it from $data. Notice that CTable was made for showing database results, if the array structure is changed CTable will break.</p>
 </body>
 </html>
